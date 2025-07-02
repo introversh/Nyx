21d97a2bb8d674e87ef355d7800d515317f42328
Shivankar <introversh@gmail.com>
2025-07-02 16:07:51 +0530

    Minor Changes

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,6 +1,9 @@
 import { MessageSquare } from "lucide-react";
-
+import { useAuthStore } from "../store/useAuthStore";
 const NoChatSelected = () => {
+    const authUser = useAuthStore((state) => state.authUser);
+  const name = authUser?.fullName;
+console.log(authUser)
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
@@ -15,9 +18,9 @@ const NoChatSelected = () => {
           </div>
         </div>
 
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">welcome, {name}!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+        
         </p>
       </div>
     </div>
