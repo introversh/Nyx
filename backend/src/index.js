61ac19630dc05662c28dbbe92fe51922b5cb2c65
introversh <168207800+introversh@users.noreply.github.com>
2026-02-07 22:40:49 +0530

    fixed render url slash

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser())
 app.use(cors(
     {
-        origin:"https://chatwithnyx.onrender.com/",
+        origin:"https://chatwithnyx.onrender.com",
         credentials:true
     }
 ))
