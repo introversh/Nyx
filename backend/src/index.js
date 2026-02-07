cc0ad4ec58f77b3b3697579c2a99712f5d0aeb08
introversh <168207800+introversh@users.noreply.github.com>
2026-02-07 22:37:58 +0530

    minor changes for cors issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser())
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"https://chatwithnyx.onrender.com/",
         credentials:true
     }
 ))
