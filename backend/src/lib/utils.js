4a9e7268a8669d643d99f9b7cb1a9b0f732c3a2f
introversh <168207800+introversh@users.noreply.github.com>
2026-02-10 14:16:21 +0530

    secure cookie issue solved

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {maxAge:7*24*60*60*1000,
     httpOnly:true,
     sameSite:"none",
-    secure:"true"
+    secure:true
   })
   return token;
 };
