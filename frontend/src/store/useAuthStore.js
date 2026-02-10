c22d935bce9b9287c0c69e0946b50902bbcb6560
introversh <168207800+introversh@users.noreply.github.com>
2026-02-10 14:04:31 +0530

    Fixed Cors Issue

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast"
 import axios from "axios"
 import {io} from  "socket.io-client"
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001":"/"
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001":"https://nyx-6amq.onrender.com"
 
 export const useAuthStore = create((set,get)=>({
     authUser:null,
